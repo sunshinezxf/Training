12379917bfd36bc7a05603229a47c0048429243c
xufan.zhang <sunshinezxf@hotmail.com>
2018-08-08 15:05:59 +0800

    fix dependecy error

---
+++ knowlege/src/main/java/finley/training/controller/DomainController.java
@@ -0,0 +1,11 @@
+package finley.training.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Each domain is regarded as a specific knowledge
+ */
+@RestController
+public class DomainController {
+
+}
