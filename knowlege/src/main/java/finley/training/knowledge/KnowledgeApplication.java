2748e87bbac962a40cd45fdc52b6c0582b11c182
hushenghao <295065492@qq.com>
2018-08-14 10:05:14 +0800

    correct some problems in maven dependency and add something

--- knowlege/src/main/java/finley/training/knowledge/KnowledgeApplication.java
+++ knowlege/src/main/java/finley/training/knowledge/KnowledgeApplication.java
@@ -2,7 +2,9 @@ package finley.training.knowledge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({"finley.training.controller", "finley.training.service", "finley.training.dao"})
 @SpringBootApplication
 public class KnowledgeApplication {
     public static void main(String[] args) {
