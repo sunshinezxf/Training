a78f0898434151b5996e91f711a581048b41de47
141190114@smail.nju.edu.cn <40664153+lawrenceZL@users.noreply.github.com>
2018-08-16 16:53:49 +0800

    define

--- knowlege/src/main/java/finley/training/controller/DomainController.java
+++ knowlege/src/main/java/finley/training/controller/DomainController.java
@@ -1,11 +1,14 @@
 package finley.training.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Each domain is regarded as a specific knowledge
  */
 @RestController
+@RequestMapping("/knowledge")
 public class DomainController {
+
 
 }
